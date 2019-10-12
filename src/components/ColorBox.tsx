045cf29fd28bd2869b1237475824b28c0400c91f
Daniel Ko <danko56666@gmail.com>
2019-10-11 22:45:36 -0400

    setup colorbox component

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { colorBox } from "../types/colorbox";
 
-const ColorBox = ({ background }: any) => {
+const ColorBox = ({ background, name }: colorBox) => {
     return (
         <div style={{ background: background }} className="ColorBox">
+            <span>{name}</span>
             <span>More</span>
         </div>
     );
---
+++ src/types/colorbox.ts
@@ -0,0 +1,4 @@
+export interface colorBox {
+    background: string;
+    name: string;
+}
