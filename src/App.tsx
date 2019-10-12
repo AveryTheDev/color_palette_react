f5133ca4ff1ad2ba62e4316877997f7177860616
Daniel Ko <danko56666@gmail.com>
2019-10-12 01:29:46 -0400

    add color helper functions to generate colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { seedColors } from "./seedColors";
 import Palette from "./components/Palette/Palette";
+import { generatePalette } from "./Utility/colorHelpers";
 
 const App: React.FC = () => {
+    console.log(generatePalette(seedColors[4]));
     return (
         <div>
             <Palette {...seedColors[4]} />
---
+++ src/Utility/colorHelpers.ts
@@ -0,0 +1,63 @@
+import chroma from "chroma-js";
+import { iSeedColors } from "../types/seedColors";
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+interface iGeneratePalette {
+    paletteName: string;
+    id: string;
+    emoji?: string;
+    colors: {
+        [index: number]: Array<{
+            name: string;
+            id: string;
+            hex: string;
+            rgb: string;
+            rgba: string;
+        }>;
+    };
+}
+
+export function generatePalette(starterPalette: iSeedColors) {
+    let newPalette: iGeneratePalette = {
+        paletteName: starterPalette.paletteName,
+        id: starterPalette.id,
+        emoji: starterPalette.emoji,
+        colors: {}
+    };
+    for (let level of levels) {
+        newPalette.colors[level] = [];
+    }
+    for (let color of starterPalette.colors) {
+        let scale = getScale(color.color, 10).reverse();
+        for (let i in scale) {
+            newPalette.colors[levels[i]].push({
+                name: `${color.name} ${levels[i]}`,
+                id: color.name.toLowerCase().replace(/ /g, "-"),
+                hex: scale[i],
+                rgb: chroma(scale[i]).css(),
+                rgba: chroma(scale[i])
+                    .css()
+                    .replace("rgb", "rgba")
+                    .replace(")", ",1.0)")
+            });
+        }
+    }
+    return newPalette;
+}
+function getRange(hexColor: string) {
+    const end = "#fff";
+    return [
+        chroma(hexColor)
+            .darken(1.4)
+            .hex(),
+        hexColor,
+        end
+    ];
+}
+
+function getScale(hexColor: string, numberOfColors: number) {
+    return chroma
+        .scale(getRange(hexColor))
+        .mode("lab")
+        .colors(numberOfColors);
+}
